d12251ea8fe7bc8e7c97e33a65f163835de845b7
Pedro <pedrogon3322@gmail.com>
2023-10-02 17:49:41 -0300

    confirm

--- Operadores/src/Operadores.java
+++ Operadores/src/Operadores.java
@@ -1,5 +1,42 @@
 public class Operadores{
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+       int a, b;
+
+       a = 7;
+       b = 6;
+
+       String result = a>b ? "verdadeiro" : "falso";
+
+     
+
+        System.out.println(result);
+
+        boolean condicao1 = true;
+        boolean condicao2 = false;
+        
+        if(condicao1 && condicao2){
+            System.out.println("sao verdadeiras");
+        }
+        
+        if(condicao1 || condicao2){
+            System.out.println("uma das duas sao verdadeiras");
+   
+   
     }
+
+    }
+
+
+
+
+
+
+
+
+
 }
+
+
+
+
