d058d6c66ba251e03ecdaf3f12f723a3be58dd46
Pedro <pedrogon3322@gmail.com>
2023-10-03 18:10:26 -0300

    confirm

--- Estudo/estruturasRepetição/src/estruturasRepetiçao.java
+++ Estudo/estruturasRepetição/src/estruturasRepetiçao.java
@@ -1,7 +1,23 @@
 public class estruturasRepetiçao {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+        
+        // String alunos [] = {"FELIPE", "JONAS", "JULIA", "MARCOS"};
+
+       // for (int x=0; x < alunos.length; x++){
+
+          //      System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
+     //   }
         
 
-        
+            String alunos[] = {"FELIPE", "JONAS", "JULIA", "MARCOS"};
+
+                for (String aluno : alunos){
+                    System.out.println("Nome do aluno é: " + aluno);
+                }
+
+
+
+
     }
+
 }
