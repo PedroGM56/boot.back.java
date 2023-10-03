f4ed3b7fd7aa2b48ab63b895f7d61d0f16792dde
Pedro <pedrogon3322@gmail.com>
2023-10-03 19:20:35 -0300

    confirm

--- Desafios e projetos/ControleDeFluxo/src/Contador.java
+++ Desafios e projetos/ControleDeFluxo/src/Contador.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Contador
 {
     public static void main(String[] args) throws Exception {
-        
+        Scanner terminal = new Scanner(System.in);
+            System.out.println("Digite o primeiro parâmetro: ");
+        int parametroUm = terminal.nextInt();
+            System.out.println("Digite o segundo parâmetro: ");
+        int parametroDois = terminal.nextInt();
+
+        int contagem = parametroDois - parametroUm;
+            System.out.println("O parâmetro entre os números é: " + contagem);
+
+            for(int contagem; )
     }
 }
