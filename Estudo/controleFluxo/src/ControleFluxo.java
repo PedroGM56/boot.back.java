bd6849be3bfae5a65cc171529e0e89e36f582e0e
Pedro <pedrogon3322@gmail.com>
2023-10-03 15:33:49 -0300

    confirm

--- Estudo/controleFluxo/src/ControleFluxo.java
+++ Estudo/controleFluxo/src/ControleFluxo.java
@@ -1,5 +1,14 @@
 public class ControleFluxo {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+
+        int nota = 5;
+
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota <7 ? "Recuperação" : "Reprovado";
+            System.out.println(resultado);
+            
+   
+
+
     }
+    
 }
---
+++ Estudo/controleFluxo/src/switchCase.java
@@ -0,0 +1,21 @@
+public class switchCase{
+    public static void main(String[] args) {
+      
+       String plano = "B";
+       
+       switch(plano){
+            case "T":{
+                System.out.println("5gb Youtube");
+            }
+       
+       case "M":{
+            System.out.println("Whats e instagram grátis");
+        
+       }
+       case "B":{
+            System.out.println("100 minutos de ligação");
+       
+}
+       }
+    }
+}
