29d8b9346b14540587043a7fef93bf1bfdf2ded8
Pedro <pedrogon3322@gmail.com>
2023-10-04 17:28:21 -0300

    confirm

--- Desafios e projetos/ControleDeFluxo/src/Contador.java
+++ Desafios e projetos/ControleDeFluxo/src/Contador.java
@@ -2,16 +2,37 @@ import java.util.Scanner;
 
 public class Contador
 {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws ParametrosInvalidosException {
         Scanner terminal = new Scanner(System.in);
-            System.out.println("Digite o primeiro parâmetro: ");
+     
+        System.out.println("Digite o primeiro parâmetro: ");
         int parametroUm = terminal.nextInt();
-            System.out.println("Digite o segundo parâmetro: ");
+       
+           
+        System.out.println("Digite o segundo parâmetro: ");
         int parametroDois = terminal.nextInt();
+
+        terminal.close();
+
+            if(parametroDois < parametroUm){
+                throw new ParametrosInvalidosException();
+             } else{
 
         int contagem = parametroDois - parametroUm;
             System.out.println("O parâmetro entre os números é: " + contagem);
+           
 
-            for(int contagem; )
-    }
+            for(int numeroContagem = 1; numeroContagem<=contagem; numeroContagem++){
+                System.out.println("O numero impresso é: " + numeroContagem);
+            }
+      
+            
+            
+        }
+        
+        
+      
+            
+    
+}
 }
---
+++ Desafios e projetos/ControleDeFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,9 @@
+public class ParametrosInvalidosException extends Exception{
+
+       public String aviso;
+       ParametrosInvalidosException(){
+        System.out.println("O parametro dois deve ser maior que o parametro um");
+       }
+        }
+    
+
